b6386e7fcbe2ea61d357796916398b40b1e28b29
Fedrick Siagian <superiorsiagian@gmail.com>
2021-03-07 23:58:08 +0700

    modif template

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.v_template');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {

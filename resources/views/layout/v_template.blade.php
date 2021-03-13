a1fb9da4b83bcd8a04be4f52d5c08e7a6bf42bf1
Fedrick Siagian <superiorsiagian@gmail.com>
2021-03-13 21:19:30 +0700

    update

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -55,13 +55,13 @@
       </li>
       <li class="nav-item dropdown user-menu">
         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-          <img src="{{ asset('template/') }}/dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2" alt="User Image">
+          <img src="{{ url('foto/'.Auth::user()->foto) }}" class="user-image img-circle elevation-2" alt="User Image">
           <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
         </a>
         <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <!-- User image -->
           <li class="user-header bg-primary">
-            <img src="{{ asset('template/') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ url('foto/'.Auth::user()->foto) }}" class="img-circle elevation-2" alt="User Image">
 
             <p>
               {{ Auth::user()->name }}

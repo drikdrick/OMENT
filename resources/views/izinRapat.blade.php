1ff4be5b9009cb11e3a6f396f3288b0139d03fcd
Fedrick Siagian <superiorsiagian@gmail.com>
2021-07-26 18:43:20 +0700

    tolak undangan rapat

---
+++ resources/views/izinRapat.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Dear Bapak/Ibu Ketua Prodi D3 Teknologi Informasi,</p>
+    <br>
+    <p>Ini adalah email pemberitahuan bahwa anggota rapat dengan deskripsi sebagai berikut :</p>
+    <table>
+        <tr>
+            <td>
+                Nama
+            </td>
+            <td>
+                : {{ $nama }}
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Email
+            </td>
+            <td>
+                : {{ $email }}
+            </td>
+        </tr>
+    </table>
+    <br>
+    <p>Tidak dapat menghadiri rapat dengan deksripsi sebagai berikut:</p>
+    <table>
+        <tr>
+            <td>Judul</td>
+            <td>: {{ $judul }}</td>
+        </tr>
+        <tr>
+            <td>Ketua Rapat</td>
+            <td>: {{ $ketua }}</td>
+        </tr>
+        <tr>
+            <td>Notulis</td>
+            <td>: {{ $notulis }}</td>
+        </tr>
+        <tr>
+            <td>Tempat</td>
+            <td>: {{ $tempat }}</td>
+        </tr>
+        <tr>
+            <td>Tanggal</td>
+            <td>: {{ $tanggal }}</td>
+        </tr>
+        <tr>
+            <td>Waktu</td>
+            <td>: {{ $waktu }}</td>
+        </tr>
+    </table>
+
+    <p>Untuk informasi lebih detail tentang anggota rapat dapat dilihat di <a href="http://127.0.0.1:8000/meeting/jadwal/{{ $id }}">sini</a>. </p>
+    <p>Terima kasih atas perhatian nya.</p>
+</body>
+</html>
